0762c8c6e113dc87dcfd47ffc9d2cce4f287d664
noahfranco <noahfra@gmail.com>
2019-08-06 16:17:00 -0400

    added class component and set consturctor and state. Alos, created a file called components and added the files I need

---
+++ github-user-card/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component() {
+constructor() {
+  super(); 
+  this.state = {
+    gitHub: [] 
+  };
+}
+
+
+render() {
+  return (
+    <div> 
+      
+    </div>
+  );
+}
+}
+  
+
+export default App;
