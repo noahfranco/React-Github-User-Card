b9779f3fc5cb4b0df252d5da2fdb2976aae60c44
noahfranco <noahfra@gmail.com>
2019-08-06 16:24:34 -0400

    moved function to the right file

--- github-user-card/src/component/Card.js
+++ github-user-card/src/component/Card.js
@@ -1,1 +1,9 @@
 import React from "react"; 
+
+fetchUsers = () => {
+    fetch("https://api.github.com/noahfranco/")
+    .then(res => {
+        return res.json(); 
+    })
+    .then(users => this.setState)
+}
