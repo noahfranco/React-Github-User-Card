4873f9833ab2502da3b5fd9bac1be4b17583f527
noahfranco <noahfra@gmail.com>
2019-08-06 23:04:51 -0400

    finished rendering UserCard.js file to the page

--- github-user-card/src/component/UserCard.js
+++ github-user-card/src/component/UserCard.js
@@ -6,7 +6,9 @@ function Card(props) {
         <div>
            <img src={ props.data.avatar_url } /> 
            <div> Bio: { props.data.bio } </div>  
-           <div> Location: { props.data.location } </div>   
+           <div> Location: { props.data.location } </div> 
+           <div> Followers: { props.data.followers } </div>
+           <div> Fowllowing: { props.data.following } </div>   
          </div> 
             
 
